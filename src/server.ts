14db28addff5f5079a2afab4f8817047d9c7f1aa
Your Name <you@example.com>
2020-11-22 09:50:09 -0300

    aula 2 - conhecendo os parameters

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 
 var server = express();
-
+server.use(express.json());
 //res.end("Hello");
+//server.get("/:id", (req, res) => {
+//let user = req.query; // params //body
+//console.log(user);
+//res.json({user: "Diego"}); //});
+
 server.get("/", (req, res) => {
+    let user = req.body;
+    console.log(user);
     res.json({user: "Diego"});
 });
 
