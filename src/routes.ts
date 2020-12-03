b62e31dcc0ee4a1358c873cdb3a39c23fe997f16
Your Name <you@example.com>
2020-12-02 23:15:59 -0300

    ajustes na aplicação

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,22 @@
 import { Router } from "express";
 import MembrosController from './controllers/MembrosController';
+//para as imagens
+import multer from 'multer';
+import path from 'path';
+import {uuid} from 'uuidv4';
 
 const membrosController: MembrosController = new MembrosController(); 
-const routes = Router();
 
+//const config = multer({
+//    storage: multer.diskStorage({
+//        destination: path.join(__dirname,'..','uploads/')
+//        filename(req, file, callback){
+//            const fileNme = 
+//        }
+//    })
+//});
+
+const routes = Router();
 //router.get.. tbm daria certo.abnf
 
 routes.route('/membros')
